5f76f7078d233a10a134af9e7c540014b0902d88
Rod Johnson <rod@atomist.com>
2019-04-04 13:39:28 +1100

    First cut

--- lib/machine/machine.ts
+++ lib/machine/machine.ts
@@ -15,12 +15,19 @@
  */
 
 import {
+    onAnyPush,
+    PushImpact,
+    PushImpactListener, PushImpactResponse,
+    SdmContext,
+    slackInfoMessage,
+    slackWarningMessage,
     SoftwareDeliveryMachine,
     SoftwareDeliveryMachineConfiguration,
 } from "@atomist/sdm";
-import {
-    createSoftwareDeliveryMachine,
-} from "@atomist/sdm-core";
+import { createSoftwareDeliveryMachine, } from "@atomist/sdm-core";
+import { AllFiles, Project, projectUtils, RepoRef } from "@atomist/automation-client";
+
+import * as _ from "lodash";
 
 /**
  * Initialize an sdm definition, and add functionality to it.
@@ -36,11 +43,106 @@ export function machine(
         configuration,
     });
 
-    /*
-     * this is a good place to type
-    sdm.
-     * and see what the IDE suggests for after the dot
-     */
+    // Goal to react to any push
+    const pushImpact = new PushImpact()
+        .withListener(sniffForSecrets());
 
+    sdm.withPushRules(
+        onAnyPush().itMeans("sniff for secrets").setGoals(pushImpact),
+    );
+
+    sdm.addCodeInspectionCommand({
+        name: "secretSniffer",
+        intent: ["find secrets", "sniff secrets", "release the hound"],
+        inspection: async (p, ci) => {
+            await ci.addressChannels(`Sniffing project at ${p.id.url} for secrets`);
+            const exposedSecrets = await sniffProject(p);
+            if (exposedSecrets.length === 0) {
+                await ci.addressChannels(slackInfoMessage(p.id.url, "Everything is cool and secure :thumbsup:"));
+            } else {
+                await renderExposedSecrets(exposedSecrets, ci);
+            }
+        },
+    });
     return sdm;
+}
+
+async function renderExposedSecrets(exposedSecrets: ExposedSecret[], sdmc: SdmContext) {
+    for (const es of exposedSecrets) {
+        await sdmc.addressChannels(slackWarningMessage(es.repoRef.url,
+            `Exposed secret: ${es.description} in \`${es.path}\``,
+            sdmc.context));
+    }
+}
+
+/**
+ * On every push, scan for secrets
+ * @return {PushImpactListener<{}>}
+ */
+function sniffForSecrets(): PushImpactListener<{}> {
+    return async pil => {
+        const exposedSecrets = await sniffProject(pil.project);
+        await renderExposedSecrets(exposedSecrets, pil);
+        return {
+            response: exposedSecrets.length > 0 ?
+                PushImpactResponse.failGoals :
+                PushImpactResponse.proceed
+        };
+    };
+}
+
+export interface ExposedSecret {
+
+    repoRef: RepoRef;
+
+    /**
+     * File path within project
+     */
+    path: string;
+
+    secret: string;
+
+    description: string;
+
+    // TODO add source location extraction
+}
+
+/**
+ * Sniff this project for exposed secrets.
+ * Open every file.
+ * @param {Project} project
+ * @return {ExposedSecret[]}
+ */
+export async function sniffProject(project: Project): Promise<ExposedSecret[]> {
+    return _.flatten(await projectUtils.gatherFromFiles(project, AllFiles, async f => {
+        return sniffFileContent(project.id, f.path, await f.getContent());
+    }));
+}
+
+/**
+ * Note that all regexes must be global
+ * @type {any[]}
+ */
+const SecretPatterns: Array<{ pattern: RegExp, description: string }> = [
+    {
+        pattern: /AKIA[0-9A-Z]{16}/g,
+        description: "AWS secret",
+    },
+];
+
+export async function sniffFileContent(repoRef: RepoRef, path: string, content: string): Promise<ExposedSecret[]> {
+    const exposedSecrets: ExposedSecret[] = [];
+    for (const pat of SecretPatterns) {
+        if (!pat.pattern.flags.includes("g")) {
+            throw new Error("All regexes must be global: Found " + pat.pattern.source);
+        }
+        const matches = content.match(pat.pattern) || [];
+        matches.forEach(m => exposedSecrets.push(({
+            repoRef,
+            path,
+            description: pat.description,
+            secret: m,
+        })));
+    }
+    return exposedSecrets;
 }
